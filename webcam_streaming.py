e55fbfee593f85734ccfe9dd366981e1114f64db
David Barthelemy <davidbarthelemy28@gmail.com>
2021-10-01 22:06:50 +0200

    start stop camera

--- webcam_streaming.py
+++ webcam_streaming.py
@@ -1,39 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, g
 import cv2
 
 app = Flask("webcam_streaming")
 
+global started
 started = False
-camera = cv2.VideoCapture(0) # let's check here if you have a camera. Maybe select the camera?
+
+camera = cv2.VideoCapture(0)
 
 def gen_frames():
+    global started
+    if not started:
+        return
     while True:
-        success, frame = camera.read() 
+        success, frame = camera.read()
         if success and started:
             try:
-                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame, 1))
                 frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
             except Exception as e:
                 pass
-        else:
-            pass
 
-@app.route('/start_stop', methods=['POST'])
+
+@app.route('/start_stop_camera', methods=['POST'])
 def start_stop_webcam():
-    started = !started
+    global started, camera
+    started = not started
     if not started:
         camera.release()
         cv2.destroyAllWindows()
     else:
         camera = cv2.VideoCapture(0)
-    return Response({})
+    return render_template('index.html')
 
 @app.route('/camera')
-def video_feed():
+def camera():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
+
+camera.release()
+cv2.destroyAllWindows()
